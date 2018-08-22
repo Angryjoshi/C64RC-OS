f9154eca90415df42035b06610835af85679e58f
Curiosity Rover <CamdenKerby@gmail.com>
2018-08-22 02:05:12 -0400

    Adding the project files

---
+++ C64RCOS/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace C64RCOS
+{
+    public class Kernel: Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("    **** COMMODORE 64 BASIC V2 ****    ");
+            Console.WriteLine(" 64K RAM SYSTEM  38911 BASIC BYTES FREE");
+            Console.WriteLine("READY.");
+        }
+        
+        protected override void Run()
+        {
+            Console.Write("");
+            var input = Console.ReadLine();
+            
+            if(input == "helloworld")
+            {
+                Console.WriteLine("Hello, World!");
+            }
+
+        }
+    }
+}
