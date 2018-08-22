d65ca4e4efd55f26ac8d45cda07b8cb8312d1c98
Curiosity Rover <CamdenKerby@gmail.com>
2018-08-22 12:18:23 -0400

    More commands

--- C64RCOS/Kernel.cs
+++ C64RCOS/Kernel.cs
@@ -9,9 +9,10 @@ namespace C64RCOS
     {
         protected override void BeforeRun()
         {
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("    **** COMMODORE 64 BASIC V2 ****    ");
-            Console.WriteLine(" 64K RAM SYSTEM  38911 BASIC BYTES FREE");
+            Console.WriteLine("64K RAM SYSTEM  38911 BASIC BYTES FREE");
             Console.WriteLine("READY.");
         }
         
@@ -19,14 +20,42 @@ namespace C64RCOS
         {
             Console.Write("");
             var input = Console.ReadLine();
-            
-            if(input == "helloworld")
+
+            if (input == "helloworld")
             {
                 Console.WriteLine("Hello, World!");
             }
+            else if (input.StartsWith("print"))
+            {
+                try
+                {
+                    Console.WriteLine(input.Remove(0, 5));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (input == "reboot")
+            {
+                Sys.Power.Reboot();
+            }
+            else if (input == "clear")
+            {
+                Console.Clear();
+            }
+            else if (input == "listcmds")
+            {
+                Console.WriteLine("----Commands----");
+                Console.WriteLine("helloworld - displays Hello, World!");
+                Console.WriteLine("print - prints the text that follows after a space");
+                Console.WriteLine("reboot - restarts the computer");
+                Console.WriteLine("clear - clear the screen of text");
+                Console.WriteLine("----Commands----");
+            }
             else
             {
-                Console.WriteLine("Error: Unknown Command");
+                Console.WriteLine("?Syntax Error.");
             }
         }
     }
